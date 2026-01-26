3996cb309f1e70c9d53b65048a09c84effe8203a
Nicklas <gta5.r6s.bf4@gmail.com>
2026-01-26 13:57:12 +0100

    Add test for handling bad events in registered event dispatch

--- Tests/PluginEventRegistryTest.cs
+++ Tests/PluginEventRegistryTest.cs
@@ -173,21 +173,53 @@ public class PluginEventRegistryTest
         Assert.Equal("Handler2 Invoked", response2);
     }
 
-    private static async Task<bool> AdvancedPredicate(IMessage message)
+    [Fact]
+    public async Task RegisteredEvents_BadEventDoesNotAffectOthers()
+    {
+        // Arrange
+        var registry = new PluginEventRegistry();
+        
+        var event1 = "GoodEvent";
+        var event2 = "BadEvent";
+        var event3 = "AnotherGoodEvent";
+
+        var response1 = "";
+        var response3 = "";
+        static Task<bool> predicate(IMessage _) => Task.FromResult(true);
+        Task goodHandler1(IMessage _) => Task.Run(() => response1 = "Good Handler 1 Invoked");
+        Task badHandler(IMessage _) => Task.Run(() => throw new Exception("Handler Error"));
+        Task goodHandler3(IMessage _) => Task.Run(() => response3 = "Good Handler 3 Invoked");
+
+        registry.SubscribeToMessage(event1, predicate, goodHandler1);
+        registry.SubscribeToMessage(event2, predicate, badHandler);
+        registry.SubscribeToMessage(event3, predicate, goodHandler3);
+
+        Mock<IMessage> mockMessage = new();
+        mockMessage.SetupGet(m => m.Content).Returns("Test Message");
+
+        // Act
+        await registry.DispatchMessageAsync(mockMessage.Object);
+
+        // Assert
+        Assert.Equal("Good Handler 1 Invoked", response1);
+        Assert.Equal("Good Handler 3 Invoked", response3);
+    }
+
+    private static Task<bool> AdvancedPredicate(IMessage message)
     {
         if (message.IsFromBot())
         {
-            return false;
+            return Task.FromResult(false);
         }
 
         if (message.IsUserMessage(out var userMessage) && userMessage is not null)
         {
-            if (userMessage.Content.StartsWith("!"))
+            if (userMessage.Content.StartsWith('!'))
             {
-                return true;
+                return Task.FromResult(true);
             }
         }
 
-        return false;
+        return Task.FromResult(false);
     }
 }
